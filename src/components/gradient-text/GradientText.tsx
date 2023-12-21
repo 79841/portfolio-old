c65f39aa55bdf3ad09278072a74d1a60e9946aaa
LEtMeDEv <79841@naver.com>
2023-12-22 00:39:15 +0900

    memorize gradient text

--- src/components/gradient-text/GradientText.tsx
+++ src/components/gradient-text/GradientText.tsx
@@ -1,10 +1,14 @@
+import { memo } from "react";
+
 type TGradientTextProps = {
   content: string;
 };
-export const GradientText = ({ content }: TGradientTextProps) => {
+export const GradientText = memo(function GradientText({
+  content,
+}: TGradientTextProps) {
   return (
     <div className="inline-block bg-gradient-to-r from-cyan-400 to-sky-950 bg-clip-text text-transparent">
       {content}
     </div>
   );
-};
+});
