7cd56ef75db010d0ea04cf6d1bbf79154ac546a2
LEtMeDEv <79841@naver.com>
2023-12-22 00:41:27 +0900

    add site metadata

---
+++ src/data/siteMetaData.ts
@@ -0,0 +1,8 @@
+export const siteMetaData = {
+  primaryEmail: "79841@naver.com",
+  secondaryEmail: "skj974600@gmail.com",
+  githubUrl: "https://github.com/79841",
+  blogUrl: "https://letmedev.vercel.app",
+  githubUsername: "79841",
+  username: "LEtMeDEv",
+};
