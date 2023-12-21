013e18193009a098f7814f30dda35c23df0b1f7e
LEtMeDEv <79841@naver.com>
2023-12-21 10:53:34 +0900

    add logo

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { FaCode } from "react-icons/fa6";
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="flex items-center">
+        <FaCode size={50} color={"#49dee9"} />
+        <div className="ml-4 h-6 select-none text-2xl font-semibold ">
+          {"LEtMeDEv"}
+        </div>
+      </div>
+    </Link>
+  );
+};
