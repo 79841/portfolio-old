45dd1df2ef905dca9556178039e6dec11ece4f09
LEtMeDEv <79841@naver.com>
2023-12-23 23:11:03 +0900

    update simple box component

---
+++ src/components/ui/box/SimpleBox.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+import { HTMLAttributes, forwardRef } from "react";
+
+export type TSimpleBoxProps = {
+  className?: string;
+} & HTMLAttributes<HTMLDivElement>;
+
+export const SimpleBox = forwardRef<HTMLDivElement, TSimpleBoxProps>(
+  function SimpleBox({ children, className, ...props }, ref) {
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          "flex w-fit flex-col space-y-4 rounded-[0.5rem] border bg-background p-4 shadow sm:p-8",
+          className,
+        )}
+        {...props}
+      >
+        {children}
+      </div>
+    );
+  },
+);
