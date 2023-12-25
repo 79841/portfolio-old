7709894cdae296ef074616eca1a94a28c5387f17
LEtMeDEv <79841@naver.com>
2023-12-25 22:50:42 +0900

    update js card

---
+++ src/components/cards/skill-card/JavascriptCard.tsx
@@ -0,0 +1,12 @@
+import { ImageCardWithTooltip } from "@/components/ui/cards";
+
+export const JavascriptCard = () => {
+  return (
+    <>
+      <ImageCardWithTooltip
+        defaultImage="/images/skills/javascript-original.svg"
+        alt="Javascript"
+      />
+    </>
+  );
+};
