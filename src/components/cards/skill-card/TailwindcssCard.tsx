7a31cc9579489ebe6e8c57ae068bbbfda6ad1046
LEtMeDEv <79841@naver.com>
2023-12-25 22:51:21 +0900

    add tailwindcss card

---
+++ src/components/cards/skill-card/TailwindcssCard.tsx
@@ -0,0 +1,12 @@
+import { ImageCardWithTooltip } from "@/components/ui/cards";
+
+export const TailwindcssCard = () => {
+  return (
+    <>
+      <ImageCardWithTooltip
+        defaultImage="/images/skills/tailwindcss-plain.svg"
+        alt="Tailwindcss"
+      />
+    </>
+  );
+};
