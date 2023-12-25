6158adffc4a14a25db3a8d194e42ffd862d43949
LEtMeDEv <79841@naver.com>
2023-12-25 22:51:51 +0900

    update site metadata

--- src/data/siteMetaData.ts
+++ src/data/siteMetaData.ts
@@ -5,4 +5,69 @@ export const siteMetaData = {
   blogUrl: "https://letmedev.vercel.app",
   githubUsername: "79841",
   username: "LEtMeDEv",
+  skills: {
+    frontEnd: {
+      javascript: {
+        name: "Javascript",
+        defaultImage: "/images/skills/javascript-original.svg",
+      },
+      typescript: {
+        name: "Typescript",
+        defaultImage: "/images/skills/typescript-original.svg",
+      },
+      react: {
+        defaultImage: "/images/skills/react-original.svg",
+        name: "React",
+      },
+      redux: {
+        defaultImage: "/images/skills/Redux-original.svg",
+        name: "Redux",
+      },
+      nextjs: {
+        defaultImage: "/images/skills/nextjs-original.svg",
+        darkModeImage: "/images/skills/nextjs-white.svg",
+        name: "Nextjs",
+      },
+      recoil: {
+        defaultImage: "/images/skills/recoil-logo.png",
+        name: "Recoil",
+      },
+      zustand: {
+        name: "Zustand",
+        defaultImage: "/images/skills/bear.jpg",
+      },
+      tanstackQuery: {
+        defaultImage: "/images/skills/tanstack-original.png",
+        name: "Tanstack Query",
+      },
+      tailwindcss: {
+        defaultImage: "/images/skills/tailwindcss-plain.svg",
+        name: "Tailwindcss",
+      },
+      shadcn: {
+        defaultImage: "/images/skills/shadcn-original.svg",
+        darkModeImage: "/images/skills/shadcn-white.svg",
+        name: "Shadcn",
+      },
+      jest: {
+        name: "Jest",
+        defaultImage: "/images/skills/jest-logo.png",
+      },
+      testingLibrary: {
+        defaultImage: "/images/skills/testing-library-logo.png",
+        name: "Testing Library",
+      },
+      msw: { defaultImage: "/images/skills/msw-logo.png", name: "Msw" },
+      prisma: {
+        defaultImage: "/images/skills/prisma-logo.png",
+        name: "Prisma",
+      },
+    },
+    backEnd: {
+      nodejs: {
+        defaultImage: "/images/skills/nodejs-logo.png",
+        name: "Nodejs",
+      },
+    },
+  },
 };
