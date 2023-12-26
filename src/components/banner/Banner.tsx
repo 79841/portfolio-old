9deffc0b6feb58341134ad9cb39463a7aa6c1efb
LEtMeDEv <79841@naver.com>
2023-12-26 13:16:20 +0900

    add banner

---
+++ src/components/banner/Banner.tsx
@@ -0,0 +1,18 @@
+type TBanner = {
+  title: string;
+};
+export const Banner = ({ title }: TBanner) => {
+  return (
+    <div className="flex w-full items-center justify-center">
+      <div className="flex flex-[1] justify-end">
+        <div className="h-[1px] w-[80%] bg-black sm:w-[50%] dark:bg-white"></div>
+      </div>
+      <h1 className="mx-8 flex justify-center text-2xl font-semibold sm:mx-20">
+        {title}
+      </h1>
+      <div className="flex flex-[1] justify-start">
+        <div className="h-[1px] w-[80%] bg-black sm:w-[50%] dark:bg-white"></div>
+      </div>
+    </div>
+  );
+};
