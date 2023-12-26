4648140c63811e2fc8ae079f04e69029988dbec1
LEtMeDEv <79841@naver.com>
2023-12-26 13:14:06 +0900

    add backend, mobile, deployment, status management, communication

--- src/app/_components/skills-section/SkillsSection.tsx
+++ src/app/_components/skills-section/SkillsSection.tsx
@@ -1,23 +1,40 @@
-import { SimpleBox } from "@/components/ui/boxes/SimpleBox";
-import { FrontEndSkills } from "./skill-categories/FrontEndSkills";
 import { SkillsSectionBanner } from "./SkillsSectionBanner";
+import {
+  BackEndSkills,
+  CommunicationSkills,
+  DeploymentSkills,
+  FrontEndSkills,
+  MobileAppSkills,
+  StatusManagementSkills,
+} from "./skill-categories";
 
 export const SkillsSection = () => {
   return (
-    <section className="flex w-screen justify-center sm:w-full">
+    <section id="skills" className="flex w-screen justify-center sm:w-full">
       <div className="flex h-fit w-full flex-col items-center px-2 py-32 sm:gap-20 md:w-[80%] lg:w-[70%]">
         <SkillsSectionBanner />
-        <div className="w-full">
-          <div className="flex justify-evenly gap-8">
+        <div className="w-full py-24">
+          <div className="flex flex-col justify-evenly gap-24">
             <div className="flex-[1]">
               <FrontEndSkills />
             </div>
-            {/* <div className="flex-[1]">
-              <FrontEndSkills />
+            <div className="flex-[1]">
+              <BackEndSkills />
+            </div>
+            <div className="flex flex-[1] flex-col flex-wrap justify-center gap-24 sm:flex-row">
+              <div className="flex-[1]">
+                <MobileAppSkills />
+              </div>
+              <div className="flex-[1]">
+                <DeploymentSkills />
+              </div>
+              <div className="flex-[1]">
+                <StatusManagementSkills />
+              </div>
             </div>
             <div className="flex-[1]">
-              <FrontEndSkills />
-            </div> */}
+              <CommunicationSkills />
+            </div>
           </div>
         </div>
       </div>
