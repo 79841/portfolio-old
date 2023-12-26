013fc1f560d1965840bf19ee8cb0e8e3037a920c
LEtMeDEv <79841@naver.com>
2023-12-26 13:14:51 +0900

    update front end skills

--- src/app/_components/skills-section/skill-categories/FrontEndSkills.tsx
+++ src/app/_components/skills-section/skill-categories/FrontEndSkills.tsx
@@ -1,17 +1,13 @@
 import { SkillBox } from "../SkillBox";
 import { siteMetaData } from "@/data/siteMetaData";
+import { SkillCategoryContainer } from "./SkillCategoryContainer";
 
 export const FrontEndSkills = () => {
   return (
-    <div className="flex w-full flex-col items-center">
-      <h1 className="mb-8 text-xl font-semibold">{"Front-End"}</h1>
-      {/* <SimpleBox className="grid w-full grid-cols-[repeat(auto-fill,minmax(5rem,1fr))] gap-4"> */}
-      <div className="flex flex-wrap justify-center gap-8">
-        {Object.values(siteMetaData.skills.frontEnd).map((skill) => (
-          <SkillBox key={skill.name} {...skill} />
-        ))}
-      </div>
-      {/* </SimpleBox> */}
-    </div>
+    <SkillCategoryContainer title="Font-End">
+      {Object.values(siteMetaData.skills.frontEnd).map((skill) => (
+        <SkillBox key={skill.name} {...skill} />
+      ))}
+    </SkillCategoryContainer>
   );
 };
