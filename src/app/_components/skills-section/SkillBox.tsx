741422c84bfd354f6b13be5cba9155ed868a1e53
LEtMeDEv <79841@naver.com>
2023-12-26 13:13:39 +0900

    change gap

--- src/app/_components/skills-section/SkillBox.tsx
+++ src/app/_components/skills-section/SkillBox.tsx
@@ -7,7 +7,7 @@ import React from "react";
 type TSkillBoxProps = { name: string } & Omit<TImageBoxProps, "alt">;
 export const SkillBox = ({ name, ...props }: TSkillBoxProps) => {
   return (
-    <div className="flex w-24 flex-col items-center gap-2">
+    <div className="flex w-24 flex-col items-center gap-1">
       <div className="flex justify-center rounded-lg duration-500 hover:relative hover:-translate-y-2">
         <ImageBox {...props} alt={name} />
       </div>
