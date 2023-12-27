607b68d461b56a6f39872d0739f2f9945bf54193
LEtMeDEv <79841@naver.com>
2023-12-27 23:47:06 +0900

    update experience timeline

--- src/app/_components/sections/experience-section/experience-timeline/RightSideExperience.tsx
+++ src/app/_components/sections/experience-section/experience-timeline/RightSideExperience.tsx
@@ -8,9 +8,9 @@ export const RightSideExperience = ({
   experience,
 }: TExperienceRightSideBoxProps) => {
   return (
-    <div className="flex w-[100%] justify-center pl-3 xl:pl-0">
+    <div className="flex w-[100%] justify-center pl-3 sm:pl-0">
       <div className="hidden w-[50%] border-r xl:block"></div>
-      <div className="relative flex w-[100%] items-center border-l py-4 pr-4 sm:w-[70%] sm:py-0 sm:pr-4 xl:w-[50%] xl:border-l-0">
+      <div className="relative flex w-[100%] items-center border-l py-4 pr-4 sm:w-[70%] sm:py-0 sm:pr-0 xl:w-[50%] xl:border-l-0">
         <div className="absolute -left-3 h-6 w-6 -translate-y-8 rounded-[50%] border bg-primary-foreground sm:-left-4 sm:h-8 sm:w-8"></div>
         <div className="h-0 min-w-8 -translate-y-8 border-8 border-b-transparent border-l-transparent border-t-transparent sm:min-w-12"></div>
         <ExperienceBox experience={experience} />
