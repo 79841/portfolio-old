dad3a460fa32d1850a4ccd47188be01c4458b5c2
LEtMeDEv <79841@naver.com>
2023-12-28 02:32:11 +0900

    remove section container

--- src/app/_components/sections/home-section/HomeSection.tsx
+++ src/app/_components/sections/home-section/HomeSection.tsx
@@ -6,18 +6,20 @@ import { JigglerContextProvider } from "@/context/jiggler";
 export const HomeSection = () => {
   return (
     <JigglerContextProvider>
-      <SectionContainer id="home">
-        <div className="flex flex-col sm:flex-row sm:gap-20">
-          <div className="flex flex-col items-end justify-start sm:flex-[1]">
-            <ProfileCard />
-          </div>
-          <div className="flex flex-col gap-8 overflow-x-hidden overflow-y-visible pt-8 sm:flex-[2]  min-[1800px]:overflow-visible">
-            <div>
-              <Preview />
+      <section id="home" className="flex w-screen justify-center sm:w-full">
+        <div className="flex h-fit w-full flex-col gap-20 px-2 pt-32 md:w-[80%] lg:w-[70%]">
+          <div className="flex flex-col sm:flex-row sm:gap-20">
+            <div className="flex flex-col items-end justify-start sm:flex-[1]">
+              <ProfileCard />
+            </div>
+            <div className="flex flex-col gap-8 overflow-x-hidden overflow-y-visible pt-8 sm:flex-[2]  min-[1800px]:overflow-visible">
+              <div>
+                <Preview />
+              </div>
             </div>
           </div>
         </div>
-      </SectionContainer>
+      </section>
     </JigglerContextProvider>
   );
 };
