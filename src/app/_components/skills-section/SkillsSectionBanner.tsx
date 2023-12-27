b513b563a5de749dd9a8447bf416ade92f23d536
LEtMeDEv <79841@naver.com>
2023-12-27 17:56:20 +0900

    move files

--- src/app/_components/skills-section/SkillsSectionBanner.tsx
+++
@@ -1,5 +0,0 @@
-import { Banner } from "@/components/banner";
-
-export const SkillsSectionBanner = () => {
-  return <Banner title="Skills" />;
-};
--- src/app/_components/skills-section/skill-categories/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./BackEndSkills";
-export * from "./CommunicationSkills";
-export * from "./DeploymentSkills";
-export * from "./FrontEndSkills";
-export * from "./MobileAppSkills";
-export * from "./StatusManagementSkills";
