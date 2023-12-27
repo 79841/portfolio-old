63b0aa15c1212bb3f927d41a56d598bc1575ea5e
LEtMeDEv <79841@naver.com>
2023-12-27 17:56:03 +0900

    move file

--- src/app/_components/experience-section/ExperienceSectionBanner.tsx
+++
@@ -1,5 +0,0 @@
-import { Banner } from "@/components/banner";
-
-export const ExperienceSectionBanner = () => {
-  return <Banner title="Experience" />;
-};
--- src/app/_components/home-section/intro/Intro.tsx
+++
@@ -1,5 +0,0 @@
-import { SimpleBox } from "@/components/ui/boxes/SimpleBox";
-
-export const Intro = () => {
-  return <SimpleBox>Intro</SimpleBox>;
-};
