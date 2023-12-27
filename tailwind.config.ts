6a4f7b4150d56576ad8a44ee772a958ada314e9a
LEtMeDEv <79841@naver.com>
2023-12-28 01:51:34 +0900

    add show-up animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -65,10 +65,15 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        "show-up": {
+          from: { opacity: "0" },
+          to: { opacity: "1" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "show-up": "show-up 0.5s",
       },
     },
   },
