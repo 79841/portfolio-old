e090457615912417f32d8e1af34a7893966c6c8b
LEtMeDEv <79841@naver.com>
2023-12-27 18:27:41 +0900

    add experience list

---
+++ src/data/experienceList.ts
@@ -0,0 +1,43 @@
+import { TExperienceData } from "@/types/ExperienceData";
+
+export const experienceList: TExperienceData[] = [
+  {
+    title: "공군 사이버 작전센터",
+    content: "체계 개발 및 취약점 분석",
+    period: "2018.08. ~ 2020.05.",
+  },
+  {
+    title: "2019 사이버공격방어대회 CCE 기관팀 3위",
+    period: "2019.11.",
+  },
+  {
+    title: " 네이버 버그바운티 명예의전당",
+    period: "2020.01.",
+  },
+  {
+    title: "비욘드 코딩",
+    content: "HTML, CSS, Javascript, Python 강의",
+    period: "2020.11. ~ ",
+  },
+  {
+    title: "2020 금융보안 위협분석 대회 FIESTA 우승",
+    period: "2020.12.",
+  },
+  {
+    title: "2020 시큐리티챌린지 개인정보비식별 본선",
+    period: "2020.12.",
+  },
+  {
+    title: "2021 금융보안 위협분석 대회 FIESTA 3위",
+    period: "2021.12.",
+  },
+  {
+    title: "토스페이먼츠",
+    content: "취약점진단 및 모의해킹",
+    period: "2022.04. ~ 2022.06.",
+  },
+  {
+    title: "2022 소개딩 해커톤 본선",
+    period: "2022.09.",
+  },
+];
