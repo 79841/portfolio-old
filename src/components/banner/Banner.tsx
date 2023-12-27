114f084d64d06343758a8d932b8963765fd52eb1
LEtMeDEv <79841@naver.com>
2023-12-28 02:05:50 +0900

    increase line length

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -5,13 +5,13 @@ export const Banner = ({ title }: TBanner) => {
   return (
     <div className="flex w-full items-center justify-center ">
       <div className="flex flex-[1] justify-end">
-        <div className="h-[1px] w-[80%] bg-primary sm:w-[50%]"></div>
+        <div className="w-[80%] border"></div>
       </div>
       <h1 className="mx-8 flex justify-center text-3xl font-bold sm:mx-20">
         {title}
       </h1>
       <div className="flex flex-[1] justify-start">
-        <div className="h-[1px] w-[80%]  bg-primary sm:w-[50%]"></div>
+        <div className="w-[80%] border bg-primary"></div>
       </div>
     </div>
   );
