9f05b13a694fb8352fafbd00b51a1b6f20c7bd5f
LEtMeDEv <79841@naver.com>
2023-12-28 02:05:36 +0900

    add border

--- src/app/_components/sections/skills-section/skill-categories/SkillCategoryContainer.tsx
+++ src/app/_components/sections/skills-section/skill-categories/SkillCategoryContainer.tsx
@@ -11,7 +11,9 @@ export const SkillCategoryContainer = ({
   return (
     <div className="flex w-full flex-col items-center">
       <h1 className="mb-8 text-xl font-semibold">{title}</h1>
-      <div className="flex flex-wrap justify-center gap-8">{children}</div>
+      <div className="flex flex-wrap justify-center gap-6 rounded-lg border px-2 py-8 sm:gap-8 sm:border-0 sm:p-0">
+        {children}
+      </div>
     </div>
   );
 };
