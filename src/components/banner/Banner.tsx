853d6144d5a02a5c66710d1a8976e0708b590b0e
LEtMeDEv <79841@naver.com>
2023-12-27 18:26:55 +0900

    update banner

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -3,15 +3,15 @@ type TBanner = {
 };
 export const Banner = ({ title }: TBanner) => {
   return (
-    <div className="flex w-full items-center justify-center">
+    <div className="flex w-full items-center justify-center ">
       <div className="flex flex-[1] justify-end">
-        <div className="h-[1px] w-[80%] bg-black sm:w-[50%] dark:bg-white"></div>
+        <div className="h-[1px] w-[80%] bg-primary sm:w-[50%]"></div>
       </div>
-      <h1 className="mx-8 flex justify-center text-2xl font-semibold sm:mx-20">
+      <h1 className="mx-8 flex justify-center text-3xl font-bold sm:mx-20">
         {title}
       </h1>
       <div className="flex flex-[1] justify-start">
-        <div className="h-[1px] w-[80%] bg-black sm:w-[50%] dark:bg-white"></div>
+        <div className="h-[1px] w-[80%]  bg-primary sm:w-[50%]"></div>
       </div>
     </div>
   );
