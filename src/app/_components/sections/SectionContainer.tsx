53bfc461863effad3fe05bed4086df559c3b97b9
LEtMeDEv <79841@naver.com>
2023-12-28 02:31:52 +0900

    decrease py

--- src/app/_components/sections/SectionContainer.tsx
+++ src/app/_components/sections/SectionContainer.tsx
@@ -4,7 +4,7 @@ type TSectionContainer = { id: string } & PropsWithChildren;
 export const SectionContainer = ({ id, children }: TSectionContainer) => {
   return (
     <section id={id} className="flex w-screen justify-center sm:w-full">
-      <div className="flex h-fit w-full flex-col gap-20 px-2 py-32 md:w-[80%] lg:w-[70%]">
+      <div className="flex h-fit w-full flex-col gap-20 px-2 py-12 sm:py-24 md:w-[80%] lg:w-[70%]">
         {children}
       </div>
     </section>
