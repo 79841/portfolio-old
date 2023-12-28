d01a2c737a1ad724916b765d77e9184c0c12ff7e
LEtMeDEv <79841@naver.com>
2023-12-28 22:01:05 +0900

    add image

--- src/app/_components/sections/experience-section/experience-timeline/ExperienceBox.tsx
+++ src/app/_components/sections/experience-section/experience-timeline/ExperienceBox.tsx
@@ -1,20 +1,35 @@
 import { SimpleBox } from "@/components/ui/boxes/SimpleBox";
 import React from "react";
 import { TExperienceData } from "@/types/ExperienceData";
+import Image from "next/image";
 
 type TExperienceBoxProps = {
   experience: TExperienceData;
 };
 export const ExperienceBox = ({ experience }: TExperienceBoxProps) => {
   return (
-    <SimpleBox className="h-full w-full flex-[1] xl:max-w-[35rem]">
-      <div className="text-xl font-semibold tracking-tighter">
-        {experience.title}
+    <SimpleBox className="h-full w-full flex-[1] flex-row items-center gap-4 sm:gap-6 xl:max-w-[35rem]">
+      <div className="relative min-h-12 min-w-12 overflow-hidden rounded-lg sm:h-20 sm:w-20">
+        {experience.image && (
+          <Image
+            src={experience.image}
+            alt={experience.title}
+            fill
+            className="object-contain"
+          />
+        )}
       </div>
-      {experience.content && (
-        <div className="text-muted-foreground">{experience.content}</div>
-      )}
-      <div className="text-muted-foreground">{experience.period}</div>
+      <div>
+        <div className="text-base font-semibold tracking-tighter sm:text-xl">
+          {experience.title}
+        </div>
+        {experience.content && (
+          <div className="text-sm text-muted-foreground">
+            {experience.content}
+          </div>
+        )}
+        <div className="text-sm text-muted-foreground">{experience.period}</div>
+      </div>
     </SimpleBox>
   );
 };
