75d0c9214a07d4f45a6ddb5622a96377ccd2bc46
LEtMeDEv <79841@naver.com>
2023-12-28 22:27:56 +0900

    add github link

--- src/app/_components/sections/projects-section/ProjectCard.tsx
+++ src/app/_components/sections/projects-section/ProjectCard.tsx
@@ -2,6 +2,9 @@ import { SimpleBox } from "@/components/ui/boxes/SimpleBox";
 import { TProjectData } from "@/types/ProjectData";
 import Image from "next/image";
 import { Badge } from "@/components/ui/badge";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+import { siteMetaData } from "@/data";
 
 type TProjectDataProps = {
   project: TProjectData;
@@ -9,14 +12,16 @@ type TProjectDataProps = {
 export const ProjectCard = ({ project }: TProjectDataProps) => {
   return (
     <SimpleBox className="flex max-w-[100%] items-center justify-center gap-12 sm:flex-row">
-      <div className="relative h-[20rem] w-full overflow-hidden rounded-lg bg-secondary sm:h-full sm:w-96">
-        {project.images && (
+      <div className="relative flex h-[20rem] w-full items-center justify-center overflow-hidden rounded-lg bg-secondary sm:h-full sm:w-96">
+        {typeof project.images !== "undefined" ? (
           <Image
             className="rounded object-cover object-top sm:object-contain sm:object-center"
             src={project.images[0]}
             alt={project.name}
             fill
           />
+        ) : (
+          <div className="text-xl font-bold">{project.name}</div>
         )}
       </div>
       <div className="hidden h-[80%] border sm:block"></div>
@@ -28,6 +33,20 @@ export const ProjectCard = ({ project }: TProjectDataProps) => {
           </div>
         </div>
         <div className="mb-4 text-sm">{project.content}</div>
+        {typeof project.github !== "undefined" && (
+          <div className="flex gap-2">
+            <div className="flex h-[22px] items-center">
+              <FaGithub className="text-xl" />
+            </div>
+            <div className="flex flex-wrap items-center gap-2">
+              {project.github.map((githubUrl) => (
+                <Badge key={githubUrl}>
+                  <Link href={githubUrl}>{githubUrl.split("/").at(-1)}</Link>
+                </Badge>
+              ))}
+            </div>
+          </div>
+        )}
         <div className="flex flex-wrap gap-2">
           {project.techStack.map((tech) => (
             <Badge key={tech} variant="secondary">
@@ -35,6 +54,7 @@ export const ProjectCard = ({ project }: TProjectDataProps) => {
             </Badge>
           ))}
         </div>
+
         <div className="text-sm text-muted-foreground">{project.date}</div>
       </div>
     </SimpleBox>
