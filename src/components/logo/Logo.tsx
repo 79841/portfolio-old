643800c0fb7297616ab095859b33964c2d8e0e6a
LEtMeDEv <79841@naver.com>
2023-12-28 22:04:20 +0900

    change link

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,7 +3,7 @@ import { FaCode } from "react-icons/fa6";
 
 export const Logo = () => {
   return (
-    <Link href="/">
+    <Link href="/#home">
       <div className="flex items-center">
         <FaCode size={50} color={"#49dee9"} />
         <div className="ml-4 h-6 select-none text-2xl font-semibold ">
