7f2294da76b2fd3abe5a5271c12dcd4959ccc2ae
LEtMeDEv <79841@naver.com>
2023-12-29 12:20:08 +0900

    update

--- src/app/_components/sections/skills-section/skill-categories/SkillCategoryContainer.tsx
+++ src/app/_components/sections/skills-section/skill-categories/SkillCategoryContainer.tsx
@@ -9,9 +9,9 @@ export const SkillCategoryContainer = ({
   children,
 }: TSkillCategoryContainer) => {
   return (
-    <div className="flex w-full flex-col items-center">
+    <div className="flex w-full flex-col items-center rounded-lg p-4 pb-8">
       <h1 className="mb-8 text-xl font-semibold">{title}</h1>
-      <div className="flex flex-wrap justify-center gap-6 rounded-lg border px-2 py-8 sm:gap-8 sm:border-0 sm:p-0">
+      <div className="flex flex-wrap justify-center gap-8 rounded-lg sm:p-0">
         {children}
       </div>
     </div>
