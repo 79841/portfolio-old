c9ab9dc58b9085d4d9fbb436029483c96b81faa6
LEtMeDEv <79841@naver.com>
2023-12-29 14:48:40 +0900

    add custom tooltip

--- src/components/ui/cards/ImageCardWithTooltip.tsx
+++ src/components/ui/cards/ImageCardWithTooltip.tsx
@@ -1,25 +1,15 @@
 "use client";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import { ImageBox, TImageBoxProps } from "@/components/ui/boxes/ImageBox";
 type TImageCardWithTooltipProps = TImageBoxProps;
 export const ImageCardWithTooltip = (props: TImageCardWithTooltipProps) => {
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger>
-          <div className="flex justify-center rounded-lg duration-500 hover:relative hover:-translate-y-2">
-            <ImageBox {...props} />
-          </div>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>{props.alt}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <div className="group relative flex flex-col items-center">
+      <div className="flex justify-center rounded-lg duration-500 hover:relative hover:-translate-y-2">
+        <ImageBox {...props} />
+      </div>
+      <div className="absolute top-[calc(100%-0.5rem)] z-40 flex items-center justify-center rounded-lg border bg-popover p-2 text-xs text-muted-foreground opacity-0 transition duration-500 group-hover:opacity-100">
+        {props.alt}
+      </div>
+    </div>
   );
 };
