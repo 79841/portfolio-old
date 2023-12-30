c47643a94aaeaaf9ff7f0af89aaa19dc698d2bd3
LEtMeDEv <79841@naver.com>
2023-12-31 07:37:33 +0900

    update profile image

--- src/app/_components/sections/home-section/profile-card/ProfileImage.tsx
+++ src/app/_components/sections/home-section/profile-card/ProfileImage.tsx
@@ -1,19 +1,14 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
 
 type TProfileImageProps = {
   src: string;
 };
 export function ProfileImage({ src }: TProfileImageProps) {
   return (
-    <div className="flex w-full justify-center">
-      <Avatar className="my-[30px] h-60 w-60">
-        <AvatarImage
-          className="bg-secondary object-contain p-4"
-          src={src}
-          alt="@shadcn"
-        />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
+    <div className="flex w-full justify-center py-4">
+      <div className="relative h-60 w-60 rounded-full bg-secondary">
+        <Image src={src} alt="profile" className="object-contain" fill />
+      </div>
     </div>
   );
 }
