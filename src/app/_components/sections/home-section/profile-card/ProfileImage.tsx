c1d61813374079111da9ea4802a8c075ecd7336c
LEtMeDEv <79841@naver.com>
2023-12-31 08:46:53 +0900

    update profile image to stop lazy loading

--- src/app/_components/sections/home-section/profile-card/ProfileImage.tsx
+++ src/app/_components/sections/home-section/profile-card/ProfileImage.tsx
@@ -7,7 +7,13 @@ export function ProfileImage({ src }: TProfileImageProps) {
   return (
     <div className="flex w-full justify-center py-4">
       <div className="relative h-60 w-60 rounded-full bg-secondary">
-        <Image src={src} alt="profile" className="object-contain" fill />
+        <Image
+          src={src}
+          alt="profile"
+          className="object-contain"
+          fill
+          priority
+        />
       </div>
     </div>
   );
