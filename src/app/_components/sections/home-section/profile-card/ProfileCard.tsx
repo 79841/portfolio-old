eca7efea6b96a74e11d9d56d08d6a8a0c61f85c2
LEtMeDEv <79841@naver.com>
2023-12-31 07:37:16 +0900

    tmp profile card

--- src/app/_components/sections/home-section/profile-card/ProfileCard.tsx
+++ src/app/_components/sections/home-section/profile-card/ProfileCard.tsx
@@ -1,23 +1,34 @@
 "use client";
-import { MouseEventHandler, useCallback, useState } from "react";
+import {
+  MouseEventHandler,
+  Suspense,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import { JiggleBox } from "@/components/ui/boxes/JiggleBox";
 import { ProfileCardBackward } from "./ProfileCardBackward";
 import { ProfileCardForward } from "./ProfileCardForward";
 import { cn } from "@/lib/utils";
+import { ProfileCardSkeleton } from "./ProfileCardSkeleton";
+import { FallbackController } from "@/components/fallback-controller/FallbackController";
 
 export function ProfileCard() {
   const [isFlipped, setIsFlipped] = useState(false);
   const handleFilp: MouseEventHandler<HTMLButtonElement> = useCallback((e) => {
     setIsFlipped((prev) => !prev);
   }, []);
+
   return (
     <JiggleBox>
+      {/* <FallbackController fallback={<ProfileCardSkeleton />}> */}
       <div className="group relative flex w-fit">
         <div className={cn("relative")}>
           <ProfileCardForward isFlipped={isFlipped} handleFilp={handleFilp} />
           <ProfileCardBackward isFlipped={isFlipped} handleFilp={handleFilp} />
         </div>
       </div>
+      {/* </FallbackController> */}
     </JiggleBox>
   );
 }
