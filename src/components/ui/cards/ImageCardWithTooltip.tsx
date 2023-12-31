108a610a544bcc2d42795c05b9b86632017f4a8c
LEtMeDEv <79841@naver.com>
2024-01-01 07:11:06 +0900

    update font color

--- src/components/ui/cards/ImageCardWithTooltip.tsx
+++ src/components/ui/cards/ImageCardWithTooltip.tsx
@@ -7,7 +7,7 @@ export const ImageCardWithTooltip = (props: TImageCardWithTooltipProps) => {
       <div className="flex justify-center rounded-lg duration-500 hover:relative hover:-translate-y-2">
         <ImageBox {...props} />
       </div>
-      <div className="absolute top-[calc(100%-0.5rem)] z-40 flex items-center justify-center rounded-lg border bg-popover p-2 text-xs text-muted-foreground opacity-0 transition duration-500 group-hover:opacity-100">
+      <div className="absolute top-[calc(100%-0.5rem)] z-40 flex items-center justify-center rounded-lg border bg-popover p-2 text-xs opacity-0 transition duration-500 group-hover:opacity-100">
         {props.alt}
       </div>
     </div>
